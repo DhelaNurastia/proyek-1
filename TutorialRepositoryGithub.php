5b8ca20f15ed7a82d1fd3252b94d6b5af49a02c0
dhela <dhelanurasita@gmail.com>
2025-06-02 12:37:13 +0700

    Menambahkan tutorial

---
+++ TutorialRepositoryGithub.php
@@ -0,0 +1,11 @@
+<!--  MANGGIL FOLDER REPOSITORY DARI GITHUB (BARU PERTAMA KALI) -->
+git clone https://github.com/DhelaNurastia/proyek-1.git <!-- ENTER -->
+
+<!-- Mengirim (upload) perubahan dari lokal ke GitHub -->
+git status <!-- buat liat apa aja yang udah kita buat -->
+git add .
+git commit -m "Judul apa yang kamu kerjain sebelumnya"
+git push origin main
+
+<!-- Menarik (update) perubahan terbaru dari GitHub ke lokal -->
+git pull
