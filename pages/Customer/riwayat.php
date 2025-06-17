3703f49eba10ec852cf5707ad3b5afcb67764872
jhynjngf <raditezahra750@gmail.com>
2025-06-17 23:57:39 +0700

    update

--- pages/Customer/riwayat.php
+++ pages/Customer/riwayat.php
@@ -205,37 +205,17 @@ $result = $koneksi->query($sql);
     <!-- Page Title -->
     <div class="page-title dark-background" data-aos="fade">
       <div class="container position-relative">
-        <h1>Starter Page</h1>
-        <p>
-          Esse dolorum voluptatum ullam est sint nemo et est ipsa porro placeat
-          quibusdam quia assumenda numquam molestias.
-        </p>
+        <h1>Riwayat Booking</h1>
+        <p>Lihat riwayat pemesanan mobil Anda secara lengkap dan terperinci di halaman ini.</p>
         <nav class="breadcrumbs">
           <ol>
             <li><a href="index.html">Home</a></li>
-            <li class="current">Starter Page</li>
+            <li class="current">Riwayat Booking</li>
           </ol>
         </nav>
       </div>
     </div>
     <!-- End Page Title -->
-
-    <!-- Starter Section Section -->
-    <section id="starter-section" class="starter-section section">
-      <!-- Section Title -->
-      <div class="container section-title" data-aos="fade-up">
-        <h2>Starter Section</h2>
-        <div>
-          <span>Check Our</span> <span class="description-title">Starter Section</span>
-        </div>
-      </div>
-      <!-- End Section Title -->
-
-      <div class="container" data-aos="fade-up">
-        <p>Use this page as a starter for your own custom pages.</p>
-      </div>
-    </section>
-    <!-- /Starter Section Section -->
 
     <!-- Booking History Section -->
     <section
@@ -244,7 +224,6 @@ $result = $koneksi->query($sql);
       aria-label="Booking History section"
     >
       <div class="container">
-        <h2>Booking History</h2>
         <table
           class="booking-table"
           aria-describedby="bookingHistoryDesc"
